204eabd05e27c3193cdc5cbb6c78bce5e52eae54
KimuTsuri <ktsuri@hpx.phys.ocha.ac.jp>
2019-07-12 17:17:52 +0900

    add new file

---
+++ code_for_stage/aut_stage.cxx
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unistd.h>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+
+int originStage() {
+  std::cout << "Move stage to origin+" << std::endl;
+  std::string cmd = "python stage.py -o +";
+  system(cmd.c_str() );
+  sleep(10);
+  return 0;
+}
+
+int moveStage(int n) {
+  std::cout << "Move stage for " << n << " pulses" << std::endl;
+  char cmd[200];
+  std::sprintf(cmd, "python stage.py -ml %d", n);
+  system(cmd);
+  sleep(3);
+  return 0;
+}
+ 
+int takeData() {
+  std::cout << "Taking data for 5 seconds" << std::endl;
+  system("echo 'sleeping'; sleep 5");
+}
+
+
+int main(int argc, char* argv[]) {
+  originStage();
+  for(int i=1;i<20;i++){
+
+    std::cout << "Move stage: " << std::endl;
+    moveStage(5000);
+    
+    std::cout << "Take data: " << std::endl;
+    takeData();
+
+    std::cout<<"-------------------------"<<std::endl;
+  }
+  
+  std::cout << "End of the program" << std::endl;
+  return 0;
+}
