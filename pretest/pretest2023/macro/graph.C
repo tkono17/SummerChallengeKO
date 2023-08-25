db35cf8b48f606308444831a0f577706fa8cdcd5
AOKImikan <aoki.m.2017@gmail.com>
2023-08-25 17:36:31 +0900

    plot macro

--- pretest/pretest2023/macro/graph.C
+++ pretest/pretest2023/macro/graph.C
@@ -1,10 +1,14 @@
 void graph(const int n, const int fn){
+  TCanvas *c1 = new TCanvas("canvas","c1");
   char filename[100];
   char grtitle[100];
+  char printname[100];
   sprintf(grtitle,"data %03d" ,fn);
+  sprintf(printname,"~/sc/SummerChallengeKO/pretest/pretest2023/plot/ksc%03d.png", fn);
   TGraph *gr = new TGraph(n);
   gr->SetTitle(grtitle);
   std::cout<<"t"<<std::endl;
+  std::cout<<gr->GetN()<<std::endl;
   for(int i=0; i<n; ++i){
     sprintf(filename, "ksc_%03d/ksc_%03d_%04d.root",fn,fn,i);
     auto f = TFile::Open(filename);
@@ -21,10 +25,12 @@ void graph(const int n, const int fn){
     //double p0 = func->Integral(810,870)/10000.;
     //std::cout<<p0<<std::endl;
     //double value = -std::log(p0);
-    gr->SetPoint(i , i, mean);
+    gr->SetPoint(i, i, mean);
+    std::cout<<i<<" , "<<mean<<std::endl;
     f->Close();
     delete f;
   }
   gr->SetMarkerStyle(20);
   gr->Draw("ap");
+  c1->Print(printname);
 }
--- pretest/pretest2023/macro/graphOcha.C
+++ pretest/pretest2023/macro/graphOcha.C
@@ -1,10 +1,14 @@
 void graphOcha(const int n, const int fn){
+  TCanvas *c1 = new TCanvas("canvas","c1");
   char filename[100];
   char grtitle[100];
-  sprintf(grtitle,"pretest %03d" ,fn);
+  char printname[100];
+  sprintf(grtitle,"pretest %03d", fn);
+  sprintf(printname,"../plot/pretest%03d.png", fn);
   TGraph *gr = new TGraph(n);
   gr->SetTitle(grtitle);
   std::cout<<"t"<<std::endl;
+    std::cout<<gr->GetN()<<std::endl;
   for(int i=0; i<n; ++i){
     sprintf(filename, "test_%03d_%03d.root",fn,i);
     auto f = TFile::Open(filename);
@@ -22,9 +26,17 @@ void graphOcha(const int n, const int fn){
     //std::cout<<p0<<std::endl;
     //double value = -std::log(p0);
     gr->SetPoint(i , i, mean);
+    std::cout<<i<<" , "<<mean<<std::endl;
     f->Close();
     delete f;
   }
+  auto x = gr->GetX();
+  auto y = gr->GetY();
+  for(int i = 0; i<n; ++i){
+    std::cout<<i<<" "<<x[i]<<" "<<y[i]<<std::endl;
+    //gr->Dump();
+  }
   gr->SetMarkerStyle(20);
-  gr->Draw("ap");
+  gr->Draw("al");
+  c1->Print(printname);
 }
